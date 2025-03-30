aa418fcc30ff8c5b66c621951b7ce7eb11445e12
André Rogério Bononi <61595447+arbononi@users.noreply.github.com>
2025-03-30 11:45:54 -0300

    Apenas mais algumas modificações

--- Projeto01.py
+++ Projeto01.py
@@ -1,7 +1,10 @@
 from time import sleep as s
 print()
 for i in range(10, -1, -1):
-    s(1)
     print("Irá explodir em", i, "segundos....", sep=" ")
+    if i == 5:
+       print("Você ainda tem chance de cair fora!")
+    s(1)
 
+print("Seu tempo acabou!!!")
 print("Booooooommmmmmmmm!!!!!!")
